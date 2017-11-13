2705caa54d316da17c67fa497910639408b406b0
Jaymit Desai <jaymit_123@hotmail.com>
2017-11-13 03:24:27 -0600

    Testing Google OAuth 2.0

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
 
-app.get("/", (req, res) => res.send({ hi: 'there' }));
+const credential = process.env.GoogleOAuth || require('./config/devs.json');
 
+
+
+
+passport.use(new GoogleStrategy({
+        clientID: credential.OAuthCredentials.clientID,
+        clientSecret: credential.OAuthCredentials.clientSecret,
+        callbackURL: "/auth/google/callback",
+    },
+    function(request, accessToken, refreshToken, profile, done) {
+        console.log(accessToken);
+    }
+));
+
+
+app.get("/auth/google", passport.authenticate('google', { scope: ['profile', 'email'] }));
+sta
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {});
+app.listen(PORT, () => { console.log(PORT); });
