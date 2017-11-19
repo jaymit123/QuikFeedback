dd5c4aed366ad329dc88636379399f731ffc90b3
Jaymit Desai <jaymit_123@hotmail.com>
2017-11-19 02:35:10 -0600

    Setup Proxy Routes for Development

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <a href="/auth/google">Google Sign In</a>
       </div>
     );
   }
