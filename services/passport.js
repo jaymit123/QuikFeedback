b29be6ce103fdc842b2c2060f76b4db33f6541ad
Jaymit Desai <jaymit_123@hotmail.com>
2017-11-20 01:28:26 -0600

    Using Promises to handle async request with async/await

--- services/passport.js
+++ services/passport.js
@@ -1,37 +1,33 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const credential = require('../config/keys');
-const dynamodb = require('./dynamodb');
-
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const credential = require("../config/keys");
+const dynamodb = require("./dynamodb");
 
 passport.serializeUser((user, done) => {
-    done(null, user.Item.id.S);
+  done(null, user.id.S);
 });
 
-passport.deserializeUser((id, done) => {
-    dynamodb.getUserByUID(id, (err, data) => {
-        if (data) {
-            done(null, data);
-        } else {
-            console.log("hi" + err);
-        }
-    })
+passport.deserializeUser(async (id, done) => {
+  try {
+    let user_entry = await dynamodb.getUserByUID(id);
+    done(null,user_entry);
+  } catch (er) {
+    console.log(er);
+  }
 });
 
-
-
-
-
-passport.use(new GoogleStrategy({
-        clientID: credential.OAuthCredentials.clientID,
-        clientSecret: credential.OAuthCredentials.clientSecret,
-        callbackURL: "/auth/google/callback",
-        proxy: true
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: credential.OAuthCredentials.clientID,
+      clientSecret: credential.OAuthCredentials.clientSecret,
+      callbackURL: "/auth/google/callback",
+      proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
-        let email = profile.emails[0]['value'];
-        let googleId = profile.id;
-        dynamodb.accountCreate(googleId, email, done);
-
+      let email = profile.emails[0]["value"];
+      let googleId = profile.id;
+      dynamodb.accountCreate(googleId, email, done);
     }
-));
+  )
+);
